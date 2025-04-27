ebc9a8034dffc3ef6fea11357d68a350997d7e2d
rachit976 <mukeshsoni38@gmail.com>
2025-04-27 19:51:31 +0530

    Add files via upload

---
+++ number1 =[1 2 3].py
@@ -0,0 +1,12 @@
+number1 =[1 2 3]
+number2 =[4,5 6]
+
+result = map (lamba x,y: x+ y, numbers1 numbers2)
+print(list(result))
+nums =[1 2 3 4 5]
+def sq (n):
+    returnn*n
+    square = list(map(sq,nums))
+    print('squareof numbers in list')
+    print(square)
+    
